fdaf843d99c6c0d90b15354ef97a8a61f6ae3143
alexmasluk <alex.masluk@gmail.com>
2014-09-21 12:03:41 -0700

    prob 4 update

--- CPSC121/Lab4/Problem4.cpp
+++ CPSC121/Lab4/Problem4.cpp
@@ -1,18 +1,20 @@
 //=========================================================================
-// File: problem1.cpp
+// File: problem4.cpp
 //=========================================================================
 // Programmer: Alex Masluk
 // Date: 09/25/2014
 // Class: CPSC 121 ("Programming Concepts")
 // Time: Tues 3:30PM
 // Instructor: Dr. Ray Ahmadnia
-// Project: Project No. 4, Problem No. 2
+// Project: Project No. 4, Problem No. 4
 // Description:
-//
+//      Read input from a text file into an array
 //
 //=========================================================================
 
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 //====== main =============================================================
@@ -20,7 +22,18 @@ using namespace std;
 //=========================================================================
 int main()
 {
+    ifstream inputFile;
+    inputFile.open("names.txt");
 
+    string line;
+    if (inputFile.is_open())
+    {
+        while (getline (inputFile, line))
+        {
+            cout << line << endl;
+        }
+    }
+    inputFile.close();
 
 //    system("pause");
 	return 0;
